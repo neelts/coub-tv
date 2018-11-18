025715d408943b475ffb3f3a0f0596c2749a6851
neelts <neeelts@gmail.com>
2018-11-18 16:19:07 +0300

    First version. Android TV DPad controls. Android TV Banner. Mobile/Tablet tap to next/prev.

--- mobile/src/main/java/neelts/coubs/FullscreenActivity.kt
+++ mobile/src/main/java/neelts/coubs/FullscreenActivity.kt
@@ -1,12 +1,16 @@
 package neelts.coubs
 
 import android.Manifest
+import android.app.Activity
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
-import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.Window
+import android.view.WindowManager
 import com.github.kittinunf.fuel.android.extension.responseJson
 import com.github.kittinunf.fuel.httpGet
 import com.google.android.exoplayer2.ExoPlayerFactory
@@ -16,22 +20,31 @@ import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_fullscreen.*
+import neelts.coubs.api.Coubs
 import neelts.coubs.api.Timeline
+import java.io.File
 import kotlin.concurrent.thread
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-class FullscreenActivity : AppCompatActivity() {
+class FullscreenActivity : Activity() {
 
 	companion object {
 		const val COUB = "http://coub.com/api/v2"
 		const val WRITE = Manifest.permission.WRITE_EXTERNAL_STORAGE
+		const val AUDIO = "mp3"
+		const val VIDEO = "mp4"
 	}
 
-	private var audioUri: Uri? = null
-	private var videoUri: Uri? = null
+	private var index: Int = 0
+	private var page: Int = 1
+
+	private var coubs: Array<Coubs> = emptyArray()
+
+	private var audioSource: ExtractorMediaSource? = null
+	private var videoSource: ExtractorMediaSource? = null
 
 	private lateinit var audioPlayer: SimpleExoPlayer
 	private lateinit var videoPlayer: SimpleExoPlayer
@@ -40,9 +53,9 @@ class FullscreenActivity : AppCompatActivity() {
 
 		super.onCreate(savedInstanceState)
 
+		requestWindowFeature(Window.FEATURE_NO_TITLE)
+		window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 		setContentView(R.layout.activity_fullscreen)
-		supportActionBar?.setDisplayHomeAsUpEnabled(true)
-		supportActionBar?.hide()
 
 		audioPlayer = ExoPlayerFactory.newSimpleInstance(applicationContext)
 		audioPlayer.playWhenReady = true
@@ -52,8 +65,20 @@ class FullscreenActivity : AppCompatActivity() {
 		video.useController = false
 		videoPlayer.playWhenReady = true
 
-		audioPlayer.addListener(AudioLoopListener(audioPlayer, videoPlayer))
-		videoPlayer.addListener(VideoLoopListener(videoPlayer))
+		audioPlayer.addListener(object : Player.EventListener {
+			override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+				if (playbackState == Player.STATE_ENDED) {
+					audioPlayer.seekTo(0)
+					videoPlayer.seekTo(0)
+				}
+			}
+		})
+
+		videoPlayer.addListener(object : Player.EventListener {
+			override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+				if (playbackState == Player.STATE_ENDED) videoPlayer.seekTo(0)
+			}
+		})
 
 		if (ContextCompat.checkSelfPermission(this, WRITE) != PackageManager.PERMISSION_GRANTED) {
 			if (ActivityCompat.shouldShowRequestPermissionRationale(this, WRITE)) {
@@ -70,50 +95,78 @@ class FullscreenActivity : AppCompatActivity() {
 		start()
 	}
 
+	private fun getNext() {
+		audioSource = null
+		videoSource = null
+		stop()
+		thread { getMedia() }.join()
+		start()
+	}
+
 	private fun start() {
-		if (videoUri != null && audioUri != null) {
-			audioPlayer.prepare(buildMediaSource(audioUri))
-			videoPlayer.prepare(buildMediaSource(videoUri))
+		if (videoSource != null && audioSource != null) {
+			audioPlayer.prepare(audioSource)
+			videoPlayer.prepare(videoSource)
 		}
 	}
 
-	private fun getHot() {
-		"$COUB/timeline/hot".httpGet().responseJson().third.fold(
-			{ coubs -> getURI(Gson().fromJson(coubs.content, Timeline::class.java)) },
+	private fun getHot(media: Boolean = true) {
+		"$COUB/timeline/hot?page=$page".httpGet().responseJson().third.fold(
+			{ response -> run {
+				val timeline = Gson().fromJson(response.content, Timeline::class.java)
+				coubs = coubs.plus(timeline.coubs)
+				if (media) getMedia()
+			} },
 			{ error -> println(error.toString()) }
 		)
 	}
 
-	private fun getURI(timeline: Timeline) {
-		val coub = timeline.coubs[0]
-		val html5 = coub.file_versions.html5
-		var url = html5.video.high?.url
-		if (url == null) url = html5.video.med?.url
-		url?.httpGet()?.response()?.third?.fold(
-			{ data -> patch(coub.id.toString(), data) },
-			{ error -> println(error.toString()) }
-		)
-		url = html5.audio.high?.url
-		if (url == null) url = html5.audio.med?.url
-		url?.httpGet()?.response()?.third?.fold(
-			{ data -> saveAudio(coub.id.toString(), data) },
-			{ error -> println(error.toString()) }
-		)
+	private fun getMedia() {
+
+		if (index < coubs.size) {
+
+			val coub = coubs[index]
+			val html5 = coub.file_versions.html5
+			val file = coub.id.toString()
+
+			val videoFile = getFile(file, VIDEO)
+			if (videoFile.exists()) {
+				videoSource = buildMediaSource(Uri.fromFile(videoFile))
+			} else {
+				var url = html5.video.high?.url
+				if (url == null) url = html5.video.med?.url
+				url?.httpGet()?.response()?.third?.fold(
+					{ data -> saveVideo(file, data) },
+					{ error -> println(error.toString()) }
+				)
+			}
+
+			val audioFile = getFile(file, AUDIO)
+			if (audioFile.exists()) {
+				audioSource = buildMediaSource(Uri.fromFile(audioFile))
+			} else {
+				var url = html5.audio.high?.url
+				if (url == null) url = html5.audio.med?.url
+				url?.httpGet()?.response()?.third?.fold(
+					{ data -> saveAudio(file, data) },
+					{ error -> println(error.toString()) }
+				)
+			}
+		}
 	}
 
-	private fun patch(file: String, data: ByteArray) {
-		println(file)
+	private fun saveVideo(file: String, data: ByteArray) {
 		data[0] = 0
 		data[1] = 0
-		val videoFile = createTempFile(file, ".mp4", applicationContext.externalCacheDir)
+		val videoFile = getFile(file, VIDEO)
 		videoFile.writeBytes(data)
-		videoUri = Uri.fromFile(videoFile)
+		videoSource = buildMediaSource(Uri.fromFile(videoFile))
 	}
 
 	private fun saveAudio(file: String, data: ByteArray) {
-		val audioFile = createTempFile(file, ".mp3", applicationContext.externalCacheDir)
+		val audioFile = getFile(file, AUDIO)
 		audioFile.writeBytes(data)
-		audioUri = Uri.fromFile(audioFile)
+		audioSource = buildMediaSource(Uri.fromFile(audioFile))
 	}
 
 	private fun buildMediaSource(uri: Uri?): ExtractorMediaSource {
@@ -122,35 +175,61 @@ class FullscreenActivity : AppCompatActivity() {
 		).createMediaSource(uri)
 	}
 
+	private fun getFile(file:String, ext:String):File {
+		return File(applicationContext.externalCacheDir, "$file.$ext")
+	}
+
+	override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+		return when (keyCode) {
+			KeyEvent.KEYCODE_DPAD_LEFT -> left()
+			KeyEvent.KEYCODE_DPAD_RIGHT -> right()
+			else -> super.onKeyDown(keyCode, event)
+		}
+	}
+
+	override fun onTouchEvent(event: MotionEvent?): Boolean {
+		return when (event?.action) {
+			MotionEvent.ACTION_DOWN -> if (event.x >= video.width * .5) right() else left()
+			else -> super.onTouchEvent(event)
+		}
+	}
+
+	private fun left(): Boolean {
+		if (index > 0) index--
+		getNext()
+		return true
+	}
+
+	private fun right(): Boolean {
+		index++
+		if (index < coubs.size) {
+			getNext()
+		} else {
+			page++
+			thread { getHot(false) }.join()
+			getNext()
+		}
+		return true
+	}
+
 	override fun onStart() {
 		super.onStart()
 		start()
 	}
 
 	override fun onStop() {
+		stop()
 		super.onStop()
+	}
+
+	private fun stop() {
 		audioPlayer.stop()
 		videoPlayer.stop()
 	}
 
 	override fun onDestroy() {
-		super.onDestroy()
 		audioPlayer.release()
 		videoPlayer.release()
-	}
-}
-
-class VideoLoopListener(private val video: SimpleExoPlayer) : Player.EventListener {
-	override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
-		if (playbackState == Player.STATE_ENDED) video.seekTo(0)
-	}
-}
-
-class AudioLoopListener(private val audio: SimpleExoPlayer, private val video: SimpleExoPlayer) : Player.EventListener {
-	override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
-		if (playbackState == Player.STATE_ENDED) {
-			audio.seekTo(0)
-			video.seekTo(0)
-		}
+		super.onDestroy()
 	}
 }
--- mobile/src/main/java/neelts/coubs/api/Audio.kt
+++ mobile/src/main/java/neelts/coubs/api/Audio.kt
@@ -1,7 +1,7 @@
 package neelts.coubs.api
 
 data class Audio (
-	val high : Media,
-	val med : Media,
+	val high : Media?,
+	val med : Media?,
 	val sample_duration : Double
 )
